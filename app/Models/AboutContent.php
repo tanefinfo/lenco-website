b4de7e7feb4c89452f0d82bbac675de1f798a757
tanefinfo <tanefinfo@gmail.com>
2025-12-30 06:57:37 -0300

    new change

--- app/Models/AboutContent.php
+++ app/Models/AboutContent.php
@@ -8,20 +8,52 @@ class AboutContent extends Model
 {
     protected $fillable = [
         'lang',
+
+        // Hero
         'hero_title',
         'hero_subtitle',
+
+        // Portrait
+        'portrait_local',
+        'portrait_url',
+
+        // Profile
         'full_name',
         'role',
         'bio_short',
         'bio_long_1',
         'bio_long_2',
+
+        // Background
         'background_title',
         'background_p1',
         'background_p2',
+
+        // Oromo Work
         'oromo_work_title',
         'oromo_work_p1',
         'oromo_work_p2',
+
+        // Vision
         'vision_title',
         'vision_description',
+
+        // Dynamic Sections (JSON)
+        'achievement_points',
+        'social_links',
+        'philosophies',
+        'achievement_stats',
+        'milestones',
+    ];
+
+    /**
+     * Cast JSON columns automatically
+     */
+    protected $casts = [
+        'achievement_points' => 'array',
+        'social_links'       => 'array',
+        'philosophies'       => 'array',
+        'achievement_stats'  => 'array',
+        'milestones'         => 'array',
     ];
 }
